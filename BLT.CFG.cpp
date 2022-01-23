e5d40bdc8a83f773529b8040f8dfc5a38662153f
BulutCorbaciNow <78735325+BulutCorbaci@users.noreply.github.com>
2022-01-23 10:38:37 +0300

    Import Project

---
+++ BLT.CFG.cpp
@@ -0,0 +1,198 @@
+﻿#include <iostream>
+#include <string>
+#include <conio.h>
+
+#include "BLT.CFG.vars.h"
+#include "BLT.CFG.langs.h"
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int masterComfigQuality() 
+{
+
+    return 1;
+}
+
+int comangliaQuality()
+{
+
+    return 1;
+}
+
+int m0reQuality()
+{
+    // Has No Options
+    return 1;
+}
+
+int b4nnyQuality()
+{
+    // Has No Options
+    return 1;
+}
+
+int chrisQuality()
+{
+    // Has No Options
+    return 1;
+}
+
+int rhapsodyQuality()
+{
+    // Has No Options
+    return 1;
+}
+
+int noneQuality()
+{
+    // Has No Options
+    return 1;
+}
+
+int setGraphicConfig()
+{
+    cout << s_SelectGraphic << endl;
+    cout << "1) Mastercomfig" << endl;
+    cout << "2) Comanglia" << endl;
+    cout << "3) M0re Config" << endl;
+    cout << "4) B4nny Config" << endl;
+    cout << "5) Chris Maxframes Config" << endl;
+    cout << "6) Rhapsody's Config" << endl;
+    cout << s_GraphicNone << endl;
+    cout << s_GraphicEndText;
+    cin >> c_GraphicConfig;
+
+    if (c_GraphicConfig == "1")
+    {
+        masterComfigQuality();
+    }
+    else if (c_GraphicConfig == "2") 
+    {
+        comangliaQuality();
+    }
+    else if (c_GraphicConfig == "3")
+    {
+        m0reQuality();
+    }
+    else if (c_GraphicConfig == "4")
+    {
+        b4nnyQuality();
+    }
+    else if (c_GraphicConfig == "5")
+    {
+        rhapsodyQuality();
+    }
+    else
+    {
+        noneQuality();
+    }
+
+    return 1;
+}
+
+int setConfigName()
+{
+    cout << s_CFGName;
+    cin >> c_Name_Temp;
+    
+    if (c_Name_Temp == "") 
+    {
+        c_Name = "New Config";
+    }
+    else
+    {
+        c_Name = c_Name_Temp;
+    }
+
+    system("cls");
+
+    setGraphicConfig();
+
+    return 1;
+}
+
+int start()
+{
+    //Variables
+    //Moved To BLT.CFG.vars.h
+
+    //Function
+    
+    cout << s_Start1 << endl;
+    cout << s_Start2 << endl;
+    cout << s_Start3 << endl;
+    cout << s_Start4 << endl;
+    cout << s_Start5 << endl;
+    cout << s_Start6 << endl;
+    cout << s_Start7 << endl;
+    cout << s_Ready1 << endl;
+    _getch();
+    system("cls");
+    setConfigName();
+
+    return 1;
+}
+
+int main()
+{
+    system("color 17");
+    cout << s_LanguageSelect_Default << endl;
+    cout << s_LanguageSupported_Default << endl;
+    cout << "1) English (Default)" << endl;
+    cout << "2) Turkce" << endl;
+    cout << s_GraphicEndText_Eng;
+    cin >> c_Language;
+
+    if (c_Language == "2")
+    {
+        s_Start1 = s_Start1_Tr;
+        s_Start2 = s_Start2_Tr;
+        s_Start3 = s_Start3_Tr;
+        s_Start4 = s_Start4_Tr;
+        s_Start5 = s_Start5_Tr;
+        s_Start6 = s_Start6_Tr;
+        s_Start7 = s_Start7_Tr;
+        s_Ready1 = s_Ready1_Tr;
+        s_CFGName = s_CFGName_Tr;
+        s_SelectGraphic = s_SelectGraphic_Tr;
+        s_GraphicEndText = s_GraphicEndText_Tr;
+        s_GraphicNone = s_GraphicNone_Tr;
+        s_Mastercomfig_High = s_Mastercomfig_High_Tr;
+        s_Mastercomfig_Low = s_Mastercomfig_Low_Tr;
+        s_Mastercomfig_Medium = s_Mastercomfig_Medium_Tr;
+        s_Mastercomfig_MediumHigh = s_Mastercomfig_MediumHigh_Tr;
+        s_Mastercomfig_MediumLow = s_Mastercomfig_MediumLow_Tr;
+        s_Mastercomfig_Ultra = s_Mastercomfig_Ultra_Tr;
+        s_Mastercomfig_VeryLow = s_Mastercomfig_VeryLow_Tr;
+    }
+    else
+    {
+        s_Start1 = s_Start1_Eng;
+        s_Start2 = s_Start2_Eng;
+        s_Start3 = s_Start3_Eng;
+        s_Start4 = s_Start4_Eng;
+        s_Start5 = s_Start5_Eng;
+        s_Start6 = s_Start6_Eng;
+        s_Start7 = s_Start7_Eng;
+        s_Ready1 = s_Ready1_Eng;
+        s_CFGName = s_CFGName_Eng;
+        s_SelectGraphic = s_SelectGraphic_Eng;
+        s_GraphicEndText = s_GraphicEndText_Eng;
+        s_GraphicNone = s_GraphicNone_Eng;
+        s_Mastercomfig_High = s_Mastercomfig_High_Eng;
+        s_Mastercomfig_Low = s_Mastercomfig_Low_Eng;
+        s_Mastercomfig_Medium = s_Mastercomfig_Medium_Eng;
+        s_Mastercomfig_MediumHigh = s_Mastercomfig_MediumHigh_Eng;
+        s_Mastercomfig_MediumLow = s_Mastercomfig_MediumLow_Eng;
+        s_Mastercomfig_Ultra = s_Mastercomfig_Ultra_Eng;
+        s_Mastercomfig_VeryLow = s_Mastercomfig_VeryLow_Eng;
+    }
+    
+
+    system("cls");
+    start();
+    return 1;
+}
---
+++ BLT.CFG.langs.h
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+
+//Usable Vars
+
+string s_Start1;
+string s_Start2;
+string s_Start3;
+string s_Start4;
+string s_Start5;
+string s_Start6;
+string s_Start7;
+string s_Ready1;
+string s_CFGName;
+string s_SelectGraphic;
+string s_GraphicNone;
+string s_GraphicEndText;
+string s_Mastercomfig_VeryLow;
+string s_Mastercomfig_Low;
+string s_Mastercomfig_MediumLow;
+string s_Mastercomfig_Medium;
+string s_Mastercomfig_MediumHigh;
+string s_Mastercomfig_High;
+string s_Mastercomfig_Ultra;
+
+//Defaults
+
+string s_LanguageSelect_Default = "Select Language";
+string s_LanguageSupported_Default = "Currently Supported Languages:";
+
+//English
+
+string s_Start1_Eng = "/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-";
+string s_Start2_Eng = "/-/-/-/-/-/-/-/BulutCorbaci's Config Generator/-/-/-/-/-/-/-/-";
+string s_Start3_Eng = "/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-";
+string s_Start4_Eng = "/-/-/-/-/-/-LightWeight Local TF2 Config Generator/-/-/-/-/-/-";
+string s_Start5_Eng = "/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-";
+string s_Start6_Eng = "/-/-/-/-/-/-/-/-/-/Generator Version v1.0.1-/-/-/-/-/-/-/-/-/-";
+string s_Start7_Eng = "/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-";
+string s_Ready1_Eng = "Press Any Key to Continue";
+string s_CFGName_Eng = "Select a Config Name: ";
+
+string s_SelectGraphic_Eng = "Select a Graphics Config:";
+string s_GraphicNone_Eng = "7) No Config (Default)";
+string s_GraphicEndText_Eng = "Input: ";
+
+string s_Mastercomfig_VeryLow_Eng = "1) Very Low Quality";
+string s_Mastercomfig_Low_Eng = "2) Low Quality";
+string s_Mastercomfig_MediumLow_Eng = "3) Medium Low Quality";
+string s_Mastercomfig_Medium_Eng = "5) Medium Quality (Default)";
+string s_Mastercomfig_MediumHigh_Eng = "6) Medium High Quality";
+string s_Mastercomfig_High_Eng = "7) High Quality";
+string s_Mastercomfig_Ultra_Eng = "8) Ultra Quality";
+
+//Turkish
+
+string s_Start1_Tr = "/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-";
+string s_Start2_Tr = "/-/-/-/-/-BulutCorbaci'nin Konfigurasyon Olusturcusu/-/-/-/-/-";
+string s_Start3_Tr = "/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-";
+string s_Start4_Tr = "/-/-/-/-/-Hafif Boyutlu Yerel TF2 Konfig Jeneratoru-/-/-/-/-/-";
+string s_Start5_Tr = "/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-";
+string s_Start6_Tr = "/-/-/-/-/-/-/-/-/-Jenerator Versiyon v1.0.1-/-/-/-/-/-/-/-/-/-";
+string s_Start7_Tr = "/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-";
+string s_Ready1_Tr = "Devam Etmek icin Herhangi Bir Dugmeye Basin";
+string s_CFGName_Tr = "Konfigurasyon Ismi Secin: ";
+
+string s_SelectGraphic_Tr = "Grafik Konfigurasyonu Secin:";
+string s_GraphicNone_Tr = "7) Konfig Kullanma (Varsayilan)";
+string s_GraphicEndText_Tr = "Giris: ";
+
+string s_Mastercomfig_VeryLow_Tr = "1) Çok Düsük Kalite";
+string s_Mastercomfig_Low_Tr = "2) Düsük Kalite";
+string s_Mastercomfig_MediumLow_Tr = "3) Orta-Düsük Kalite";
+string s_Mastercomfig_Medium_Tr = "5) Orta Kalite (Varsayilan)";
+string s_Mastercomfig_MediumHigh_Tr = "6) Orta-Yuksek Kalite";
+string s_Mastercomfig_High_Tr = "7) Yuksek Kalite";
+string s_Mastercomfig_Ultra_Tr = "8) Ultra Kalite";
